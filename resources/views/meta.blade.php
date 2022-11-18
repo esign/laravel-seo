10a68c5a8a2a64ef50dd28c2ec238ae72e2615f5
dieter vanhove <dietervanhove@dieters-MacBook-Pro.local>
2022-11-18 09:51:56 +0100

    add x-default to hreflang

--- resources/views/meta.blade.php
+++ resources/views/meta.blade.php
@@ -5,5 +5,8 @@
 <link rel="next" href="{{ seo()->meta()->getNext() }}">
 <link rel="prev" href="{{ seo()->meta()->getPrev() }}">
 @foreach (seo()->meta()->getAlternateUrls() as $locale => $url)
+@if ($loop->first && count(seo()->meta()->getAlternateUrls()) > 1)
+<link rel="alternate" hreflang="x-default" href="{{ $url }}"/>
+@endif
 <link rel="alternate" hreflang="{{ $locale }}" href="{{ $url }}">
 @endforeach
